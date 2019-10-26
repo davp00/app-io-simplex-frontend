e5d46dba060b9125c430dc32bbde0caaa3db592c
davp00 <daniielvp82@gmail.com>
2019-10-26 18:34:24 -0500

    restricciones agregadas

--- src/Components/SimplexObjetiveFunction.js
+++ src/Components/SimplexObjetiveFunction.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { SimplexContext } from "../context/SimplexContext";
+import React, {Component} from 'react';
+import {SimplexContext} from "../context/SimplexContext";
 import Title from "antd/es/typography/Title";
 import TeX from '@matejmazur/react-katex';
 import {Col, InputNumber, Row} from "antd";
@@ -8,41 +8,38 @@ class SimplexObjetiveFunction extends Component {
 
 
     render() {
-        const { n_vars,FO } = this.context;
+        const {n_vars, FO} = this.context;
 
         if (n_vars === '')
-            return(<span/>)
+            return (<span/>);
 
         return (
             <div className={'mt-5 p-2'}>
                 <Title level={4}>Funcion Objetivo</Title>
                 <div className={'mt-5'}>
-
                     <Row>
-                        <Col md={3}><TeX >{`${FO} Z = `}</TeX></Col>
-                        { this.renderVars() }
+                        <Col md={3}><TeX>{`${FO} Z = `}</TeX></Col>
+                        {this.renderVars()}
                     </Row>
                 </div>
             </div>
         )
     }
 
-    renderVars()
-    {
-        const { n_vars } = this.context;
+    renderVars() {
+        const {n_vars} = this.context;
 
         let vars = [];
-        for(let i = 0 ; i < n_vars ; i++)
-        {
+        for (let i = 0; i < n_vars; i++) {
             vars.push(
                 <span key={i}>
-                    <Col  md={3} sm={12}>
+                    <Col md={3} sm={12}>
                         <InputNumber
                         />
                         <TeX>{`x_${i}`}</TeX>
                     </Col>
                     {
-                        i !== n_vars -1 && (
+                        i !== n_vars - 1 && (
                             <Col md={1}> <TeX className={''}>+</TeX></Col>
                         )
                     }
--- src/Components/SimplexRestrictions.js
+++ src/Components/SimplexRestrictions.js
@@ -1,15 +1,70 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {SimplexContext} from "../context/SimplexContext";
 import Title from "antd/es/typography/Title";
+import {Col, Form, InputNumber, Row, Select} from "antd";
+import TeX from "@matejmazur/react-katex";
 
 
-class SimplexRestrictions extends Component{
+class SimplexRestrictions extends Component {
     render() {
         return (
             <div className={'mt-5 p-2'}>
                 <Title level={4}>Restricciones</Title>
+                <div className={'mt-5'}>
+                    {
+                        this.renderRestrictions()
+                    }
+                </div>
             </div>
         );
+    }
+
+    renderRestrictions() {
+        const {n_vars, n_restrictions} = this.context;
+        let restrictions = [];
+
+        for (let y = 0; y < n_restrictions; y++) {
+            let fields = [];
+            for (let x = 0; x < n_vars; x++) {
+                fields.push(
+                    <span key={`r-${y}-${x}`}>
+                        <Col md={3} sm={12}>
+                        <InputNumber
+                        />
+                        <TeX>{`x_${x}`}</TeX>
+                    </Col>
+                        {
+                            x !== n_vars - 1 && (
+                                <Col md={1}> <TeX className={''}>+</TeX></Col>
+                            )
+                        }
+                    </span>
+                )
+            }
+            restrictions.push(
+                <Row key={`r-${y}`}>
+                    <Form>
+                        {fields}
+                        <Col md={2}>
+                            <Form.Item>
+                                <Select key={`s-${y}`} defaultValue={'<='} className={'katex2'}>
+                                    <Select.Option key={`s-${y}-<=`} value={'<='}>{'≤'}</Select.Option>
+                                    <Select.Option key={`s-${y}->=`} value={'>='}>{'≥'}</Select.Option>
+                                    <Select.Option key={`s-${y}-=`} value={'='}>{'='}</Select.Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col md={2} className={'ml-3'}>
+                            <Form.Item>
+                                <InputNumber />
+                            </Form.Item>
+                        </Col>
+                    </Form>
+                </Row>
+            );
+        }
+
+        return restrictions;
     }
 }
 
